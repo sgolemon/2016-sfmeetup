5ff9cd7467e0ede383b5214374223a69c995bf0f
Sara Golemon <pollita@php.net>
2016-12-11 17:10:39 -0800

    Accept a string

--- hello.c
+++ hello.c
@@ -18,10 +18,28 @@ PHP_FUNCTION(hello_number) {
     RETURN_LONG(42);
 }
 
+PHP_FUNCTION(hello_you) {
+    char *name;
+    size_t name_len; /* Note: In PHP5, this is of type int */
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(),
+                              "s", &name, &name_len) == FAILURE) {
+        return;
+    }
+
+    /* Easy way: php_printf("Hello %s!\n", name); */
+
+    /* Binary safe way: */
+    php_printf("Hello ");
+    PHPWRITE(name, name_len);
+    php_printf("!\n");
+}
+
 zend_function_entry hello_functions[] = {
     PHP_FE(hello_world, NULL)
     PHP_FE(hello_return, NULL)
     PHP_FE(hello_number, NULL)
+    PHP_FE(hello_you, NULL)
     PHP_FE_END
 };
 
