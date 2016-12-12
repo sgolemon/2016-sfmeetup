f7a842bc53bfdf290ffe8ff1018a3d74b5145853
Sara Golemon <pollita@php.net>
2016-12-11 16:23:30 -0800

    Add hello_world() function

--- hello.c
+++ hello.c
@@ -6,10 +6,20 @@
 
 #include "php.h"
 
+PHP_FUNCTION(hello_world) {
+    php_printf("Hello World!\n");
+}
+
+zend_function_entry hello_functions[] = {
+    PHP_FE(hello_world, NULL)
+    PHP_FE_END
+};
+
 zend_module_entry hello_module_entry = {
     STANDARD_MODULE_HEADER,
     "hello",
-    NULL, NULL, NULL, NULL, NULL, NULL,
+    hello_functions,
+    NULL, NULL, NULL, NULL, NULL,
     NO_VERSION_YET,
     STANDARD_MODULE_PROPERTIES
 };
